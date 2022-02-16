1d50c1e27ff079abe81890ae85b6b333246d2250
Marcelo Cristo Fuhr <marcelinhofuhr@gmail.com>
2022-02-16 14:01:41 -0300

    Created a Feign client

--- hr-payroll/src/main/java/com/blinkspace/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/blinkspace/hrpayroll/services/PaymentService.java
@@ -10,21 +10,17 @@ import org.springframework.web.client.RestTemplate;
 
 import com.blinkspace.hrpayroll.entities.Payment;
 import com.blinkspace.hrpayroll.entities.Worker;
+import com.blinkspace.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service //informar para o spring que é uma classe de serviço
 public class PaymentService {
 
-	@Value("${hr-worker.host}") //propriedade que está no arquivo .properties
-	private String workerHost; //valor do workerHost vale http://localhost:8001
-	
-	@Autowired //injeção de dependência do RestTemplate.
-	private RestTemplate restTemplate;
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(long workerId, int days) {
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", ""+workerId); //para usar o workerId como String, foi criado um macete para converter para String
 		
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
